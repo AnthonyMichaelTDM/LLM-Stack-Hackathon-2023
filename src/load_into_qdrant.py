959ca11cd7d6af2eb2179d5197dadde44aa9e1be
40700820 <ajhinh@gmail.com>
2023-06-03 14:30:38 -0700

    Messages also included

--- src/load_into_qdrant.py
+++ src/load_into_qdrant.py
@@ -7,43 +7,61 @@ from qdrant_client.models import Distance, PointStruct, VectorParams
 import uuid
 
 
-client = QdrantClient(url="http://localhost:6333")
+client = QdrantClient(url="http://localhost:6334")
+chat_collection = "chats"
+message_collection = "messages"
 data_path = "./data/"
 raw_path = data_path + "raw/"
 embeddings_path = data_path + "embeddings/"
 embedding_size = 1536  # ada-002
 
 
-# Create a Qdrant collection for the chats
+# Create two qdrant collections for the chats + messages
 client.recreate_collection(
-    collection_name="chats",
+    collection_name=chat_collection,
+    vectors_config=VectorParams(size=embedding_size, distance=Distance.COSINE),
+)
+client.recreate_collection(
+    collection_name=message_collection,
     vectors_config=VectorParams(size=embedding_size, distance=Distance.COSINE),
 )
 
+
 # Load the chats and embeddings
 chats_df = pd.read_csv(raw_path + "chats.csv")
-embeddings_df = pd.read_csv(embeddings_path + "chats-embeddings-ada-002.csv")
+chats_embeddings_df = pd.read_csv(embeddings_path + "chats-embeddings-ada-002.csv")
+
+# Load the messages and embeddings
+messages_df = pd.read_csv(raw_path + "messages.csv")
+messages_df_select = messages_df[(messages_df["Channel_Name"]=="mlops-questions-answered") & (messages_df["Channel_Name"]=="discussions")]
+message_embeddings_df = pd.read_csv(embeddings_path + "messages-embeddings-ada-002-001.csv")
+
 
 # Remove all chats that don't have embeddings
-missing_embeddings_df = embeddings_df[embeddings_df["embedding"].isna()]
-missing_embeddings_df_ids = missing_embeddings_df["thread_id"].tolist()
-chats_df = chats_df[~chats_df["thread_id"].isin(missing_embeddings_df_ids)]
-embeddings_df = embeddings_df[
-    ~embeddings_df["thread_id"].isin(missing_embeddings_df_ids)
+missing_chat_embeddings_df = chats_embeddings_df[chats_embeddings_df["embedding"].isna()]
+missing_chat_embeddings_df_ids = missing_chat_embeddings_df["thread_id"].tolist()
+chats_df_select = chats_df[~chats_df["thread_id"].isin(missing_chat_embeddings_df_ids)]
+chats_embeddings_df = chats_embeddings_df[
+    ~chats_embeddings_df["thread_id"].isin(missing_chat_embeddings_df_ids)
 ]
 
-# # Load the messages and embeddings
-# messages_df = pd.read_csv(raw_path + "messages.csv")
-# messages_df_select = messages_df[messages_df["Channel_Name"]=="mlops-questions-answered" or messages_df["Channel_Name"]=="discussions"]
-# message_embeddings_df = pd.read_csv(embeddings_path + "messages-embeddings-ada-002.csv")
+# Remove all messages that don't have embeddings
+missing_message_embeddings_df = message_embeddings_df[message_embeddings_df["embedding"].isna()]
+missing_message_embeddings_df_ids = missing_message_embeddings_df["message_id"].tolist()
+messages_df_select = messages_df_select[~messages_df_select["Message_Timestamp"].isin(missing_message_embeddings_df_ids)]
+messages_df_select_ids = messages_df_select["Message_Timestamp"].tolist()
+message_embeddings_df = message_embeddings_df[
+    message_embeddings_df["message_id"].isin(messages_df_select_ids)
+]
 
 # Upsert the chats into Qdrant
 max_batch_size = 20
+
 for i in range(
-    math.ceil(len(embeddings_df) / max_batch_size)
+    math.ceil(len(chats_embeddings_df) / max_batch_size)
 ):  # Upsert chats in batches
     client.upsert(
-        collection_name="chats",
+        collection_name=chat_collection,
         points=[
             PointStruct(
                 id=str(uuid.uuid4()),
@@ -53,14 +71,39 @@ for i in range(
                     "chat_text": chat_text,
                 },
             )
-            for thread_id, vector, chat_text in zip(
-                embeddings_df["thread_id"][
+            for vector, thread_id, chat_text in zip(
+                chats_embeddings_df["embedding"][
                     i * max_batch_size : (i + 1) * max_batch_size
                 ],
-                embeddings_df["embedding"][
+                chats_embeddings_df["thread_id"][
                     i * max_batch_size : (i + 1) * max_batch_size
                 ],
-                chats_df["chat_text"][i * max_batch_size : (i + 1) * max_batch_size],
+                chats_df_select["chat_text"][i * max_batch_size : (i + 1) * max_batch_size],
+            )
+        ],
+    )
+for i in range(
+    math.ceil(len(message_embeddings_df) / max_batch_size)
+):  # Upsert chats in batches
+    client.upsert(
+        collection_name=message_collection,
+        points=[
+            PointStruct(
+                id=str(uuid.uuid4()),
+                vector=json.loads(vector),
+                payload={
+                    "thread_id": thread_id,
+                    "message_text": message_text,
+                },
+            )
+            for vector, thread_id, message_text in zip(
+                message_embeddings_df["embedding"][
+                    i * max_batch_size : (i + 1) * max_batch_size
+                ],
+                messages_df_select["Thread_Timstamp"][
+                    i * max_batch_size : (i + 1) * max_batch_size
+                ],
+                messages_df_select["__Text"][i * max_batch_size : (i + 1) * max_batch_size],
             )
         ],
     )
