80cf85e6a76378f4911c47194e7d7691a06f0321
Andrew Hinh <ajhinh@gmail.com>
2023-06-03 14:31:41 -0700

    Qdrant (#18)
    * Draft
    
    * Draft of using qdrant
    
    * Messages also included

--- src/search.py
+++ src/search.py
@@ -5,9 +5,24 @@ client = QdrantClient(url="http://localhost:6333")
 
 
 query_vector = np.random.rand(1536)
+
 hits = client.search(
     collection_name="chats",
     query_vector=query_vector,
-    limit=5  # Return 5 closest points
+    limit=5,  # Return 5 closest points
 )
 print(hits)
+
+hits = client.search(
+    collection_name="messages",
+    query_vector=query_vector,
+    limit=5,  # Return 5 closest points
+)
+print(hits)
+
+
+# Steps:
+# User asks question
+# Embed and find:
+# 1. Closest chat
+# 2. Closest messages not in the closest chat
