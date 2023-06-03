bc07810c69cd26a2a2d893a77f95d20dd44734d0
Andrew Hinh <ajhinh@gmail.com>
2023-06-03 13:33:12 -0700

    Qdrant (#17)
    * Draft of using qdrant

--- src/load_into_qdrant.py
+++ src/load_into_qdrant.py
@@ -1,67 +1,66 @@
 import json
-import pandas as pd
-from tqdm import tqdm
+import math
 
+import pandas as pd
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, PointStruct, VectorParams
+import uuid
+
+
+client = QdrantClient(url="http://localhost:6333")
 data_path = "./data/"
 raw_path = data_path + "raw/"
 embeddings_path = data_path + "embeddings/"
+embedding_size = 1536  # ada-002
 
-messages_df = pd.read_csv(raw_path + "messages.csv")
-df_mlops_questions_answered = messages_df[messages_df["Channel_Name"]=="mlops-questions-answered"]
-df_discussions = messages_df[messages_df["Channel_Name"]=="discussions"]
-message_embeddings_df = pd.read_csv(embeddings_path + "messages-embeddings-ada-002.csv")
 
+# Create a Qdrant collection for the chats
+client.recreate_collection(
+    collection_name="chats",
+    vectors_config=VectorParams(size=embedding_size, distance=Distance.COSINE),
+)
+
+# Load the chats and embeddings
 chats_df = pd.read_csv(raw_path + "chats.csv")
 embeddings_df = pd.read_csv(embeddings_path + "chats-embeddings-ada-002.csv")
 
-# Create a temp index of the chats
-chats_index = {}
-for _, row in tqdm(chats_df.iterrows(), desc="Creating temporary chats index"):
-  chats_index[row['thread_id']] = row['chat_text']
+# Remove all chats that don't have embeddings
+missing_embeddings_df = embeddings_df[embeddings_df["embedding"].isna()]
+missing_embeddings_df_ids = missing_embeddings_df["thread_id"].tolist()
+chats_df = chats_df[~chats_df["thread_id"].isin(missing_embeddings_df_ids)]
+embeddings_df = embeddings_df[
+    ~embeddings_df["thread_id"].isin(missing_embeddings_df_ids)
+]
 
-# Link the chats and embeddings together
-embeddings = []
-VECTOR_SIZE = None
-for _, row in tqdm(embeddings_df.iterrows(), desc="Collecting chats and embeddings"):
-  embedding = json.loads(row['embedding'])
-  embeddings.append({"thread_id": row['thread_id'], "embedding":  embedding})
-  if not VECTOR_SIZE:
-    VECTOR_SIZE = len(embedding)
-  else:
-    assert VECTOR_SIZE==len(embedding)
+# # Load the messages and embeddings
+# messages_df = pd.read_csv(raw_path + "messages.csv")
+# messages_df_select = messages_df[messages_df["Channel_Name"]=="mlops-questions-answered" or messages_df["Channel_Name"]=="discussions"]
+# message_embeddings_df = pd.read_csv(embeddings_path + "messages-embeddings-ada-002.csv")
 
-
-
-from qdrant_client import QdrantClient
-
-client = QdrantClient(url="http://localhost:6333")
-max_num_chunks = (
-20  # TODO: Find optimal number of chunks to upsert at once
-)
+# Upsert the chats into Qdrant
+max_batch_size = 20
 for i in range(
-math.ceil(len(extracted_chunks) / max_num_chunks)
-):  # Upsert chunks in batches
-    qdrantClient.upsert(
-        collection_name=qdrant_collection,
+    math.ceil(len(embeddings_df) / max_batch_size)
+):  # Upsert chats in batches
+    client.upsert(
+        collection_name="chats",
         points=[
             PointStruct(
-                id=chunk_id,
-                vector=vector,
+                id=str(uuid.uuid4()),
+                vector=json.loads(vector),
                 payload={
-                    "resource_pid": resource_pid,
-                    "chunk": chunk,
+                    "thread_id": thread_id,
+                    "chat_text": chat_text,
                 },
             )
-            for chunk_id, chunk, vector in zip(
-                chunk_ids[
-                    i * max_num_chunks : (i + 1) * max_num_chunks
+            for thread_id, vector, chat_text in zip(
+                embeddings_df["thread_id"][
+                    i * max_batch_size : (i + 1) * max_batch_size
                 ],
-                extracted_chunks[
-                    i * max_num_chunks : (i + 1) * max_num_chunks
+                embeddings_df["embedding"][
+                    i * max_batch_size : (i + 1) * max_batch_size
                 ],
-                embeddings[
-                    i * max_num_chunks : (i + 1) * max_num_chunks
-                ],
+                chats_df["chat_text"][i * max_batch_size : (i + 1) * max_batch_size],
             )
         ],
     )
