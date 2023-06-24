684ef98bf52ddba166e7494a62604c799ee21219
Andrew Hinh <ajhinh@gmail.com>
2023-06-24 11:01:04 -0700

    Chat draft

--- src/components/chat.js
+++ src/components/chat.js
@@ -4,12 +4,23 @@ class ChatBox extends Component {
     render() {
         return (
             <div className="ChatBox">
-                <label htmlFor="chatBox">Enter your input:</label>
-                <input type="text" id="chatBox" name="chatBox"></input>
-                <button>Send</button>
+                <form novalidate className="chat-form">
+                    <label htmlFor="chat"></label>
+                    <input type="text" id="chat" name="chat" required></input>
+                    <p class="error" aria-live="polite"></p>
+                    <button class="send-button">Hmm</button>
+                </form>
             </div>
         )
     }
+
+    chatForm = document.querySelector(".chat-form");
+    chatText = document.querySelector("#chat");
+    chatTextError = document.querySelector("#chat + p.error");
+    hmmButton = document.querySelector(".send-button");
+
+    // Call /token endpoint with username and password
+    // Call /chat endpoint with conversation_id and message
 }
 
 export default ChatBox
