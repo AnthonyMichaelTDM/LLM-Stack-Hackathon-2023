0b1e2e88efee5a3dca18c32882cf7406a0ad190a
Andrew Hinh <ajhinh@gmail.com>
2023-08-14 17:48:43 -0700

    Added header comp + prettier linting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import { Analytics } from '@vercel/analytics/react';
-import './App.css'
+import { Analytics } from "@vercel/analytics/react";
+import Header from "./Header.jsx";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Project</h1>
-      <h2>Check out the repo <a href="https://github.com/andrewhinh/project">here</a>.</h2>
+      <Header />
       <Analytics />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <h1>Project</h1>
+      <h2>
+        Check out the repo{" "}
+        <a href="https://github.com/andrewhinh/project">here</a>.
+      </h2>
+    </header>
+  );
+}
+
+export default Header;
